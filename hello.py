8c28056aeba666bdb913c399c61d82cf3f82f42e
Résztvevő - 2 <t360-v2-dtpb-s02@oktatas.local>
2022-03-02 11:59:01 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,56 @@
+print("Hello")
+
+print("hello world")
+print("hello world")
+
+
+print(5) # egész literál
+print(100_000) # egész literál
+print(3.14) # lebegőpontos
+# szövegek "" közt ; számoknál tilos, mert szöveg lesz
+
+
+employee_name = "John Doe"  # name nem kezdődhet számmal, több szó _közte
+print(employee_name)
+# shift F6 - változó átnevezése
+
+age = 30
+print(age)
+age = 31   # felülírható
+print(age)
+
+color_of_eye = "blue"
+print(color_of_eye)  # mindig meg kell adni a változó értékét előbb
+
+color_of_eye_of_John = color_of_eye  # változó érték másolása
+print(color_of_eye)
+
+# lekérdezés
+print(type(5))  # int integer
+print(type(3.14))  # float - lebegőpontos szám
+print(type("Hello")) # str - string - karakterlánc
+
+age = 32
+print(type(age))
+
+age = "harminckettő"     # megváltoztattam a class-t
+print(type(age))   # str
+
+# litarálok, változóknak - van típusa - Python típusos nyelv
+# változóknál a típus változhat - dinamikus típusos nyelv
+
+
+# type hint
+salary: int = 200
+print(salary)
+salary = "kétszáz"
+print(salary)
+
+
+# VCS - share project on github - add acc - login - share hello.py
+
+
+print("Hello World")
+print("Hello World")
+print("Hello Git")
+print("Hello GitHub")
