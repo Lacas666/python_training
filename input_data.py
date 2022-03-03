6b5a3fac0149b7a23b2b1350603721cea3a996fc
Lacas666 <laszlo.cserta@telekom.com>
2022-03-03 10:10:55 +0100

    cilus

---
+++ input_data.py
@@ -0,0 +1,14 @@
+
+# kérjük be a felasz nevet
+# írjuk ki h nem lehet üres nevet megadni
+# és kérjük be még1x
+# ha nem üres írjuk ki h köszi
+
+
+name=""
+while name=="":
+    name=input("Add meg a neved: ")
+    if name=="":
+        print("nem adhatsz meg üres értéket")
+    print("köszönöm")
+
---
+++ szám.py
@@ -0,0 +1,11 @@
+
+# van egy listátok, számok összegét kiírja
+
+ossz = 0
+szamok = [3,4,6,7,23,4,5]
+for szam in szamok:
+    #ossz = szam+ossz
+    ossz += szam
+print(ossz)
+
+
