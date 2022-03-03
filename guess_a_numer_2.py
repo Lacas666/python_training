020acccd636670507f84733320abd8f718cb0c81
Lacas666 <laszlo.cserta@telekom.com>
2022-03-03 12:03:58 +0100

    javítás

--- guess_a_numer_2.py
+++ guess_a_numer_2.py
@@ -1,16 +1,18 @@
 
 print("gondolj egy számra")
 
-min = 1
-max = 10
+min_guess = 1
+max_guess = 10
 answer = "x"
+guess = 5
 while answer != "e":
-    guess = (min + max) // 2
+    guess = (min_guess + max_guess) // 2
     print("A tippem", guess)
     answer = input("k/e/n?")
     if answer == "k":
-        max = guess - 1
+        max_guess = guess - 1
     elif answer == "n":
-        min = guess + 1
+        min_guess = guess + 1
 print("a gondolt szám", guess)
+
 
